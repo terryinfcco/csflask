118eae711268a060a47fe829b8fa44074fa04254
Terry Dutcher <terryinclmich@hotmail.com>
2023-10-29 13:58:31 -0600

    Started Video 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask #  import Flask class
+from flask import Flask, render_template #  import Flask class
 
 app = Flask(__name__) # helps Flask find templates static files
+
+# List of dictionaries - dummy data
+posts = [
+    {
+        'author': 'Terry Dutcher',
+        'title': 'Blog Post 1', 
+        'content': 'First Post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jacqui Ward',
+        'title': 'Blog Post 2', 
+        'content': 'Second Post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 # You can have multiple decorators for one function so both / and /home go to the same page
 @app.route("/home")
 @app.route("/") #  oreate home page route
 def hello():
-    return 'Hello World!'
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
